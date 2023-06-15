e2a808602d0e7f7c39cfab645d1000c2d7f947c5
chaochao001 <xzichaomcgill@163.com>
2023-06-15 01:53:41 -0700

    Update the path

--- run_rea_world.sh
+++ run_rea_world.sh
@@ -1,9 +1,9 @@
 pypy3 205proj.py > results/real_world/real_word_forward.txt << EOF
-iris.data
+data/iris.data
 1
 EOF
 
 pypy3 205proj.py > results/real_world/real_word_backward.txt << EOF
-iris.data
+data/iris.data
 2
 EOF
--- run_xl.sh
+++ run_xl.sh
@@ -1,10 +1,4 @@
 pypy3 205proj.py > results/xlarge/xlarge_12_forward.txt << EOF
-xlarge12.txt
+data/xlarge12.txt
 1
-EOF
-
-
-pypy3 205proj.py > results/xlarge/xlarge_12_backward.txt << EOF
-xlarge12.txt
-2
 EOF
